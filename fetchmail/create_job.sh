a09697f25ffef9eaa9da5e43dd475c96b81dd5ea
Artem Fufaldin <artem.fufaldin@gmail.com>
2020-08-27 14:11:47 +0500

    Create create_job.sh

---
+++ fetchmail/create_job.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+LOG_PATH="/var/log/fetchmail"
+
+MAIL_PATH="/home/mail"
+MAIL_HOST="domain.local"
+MAIL_USER="postmaster"
+
+FETCH_POLL="imap.example.com"
+FETCH_PORT=143
+FETCH_PROTO="imap"
+FETCH_USER="user@example.com"
+FETCH_PASSWORD="password"
