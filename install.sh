b26756651cad820ddd73a85ef1dd13a5713d1b13
Artem Fufaldin <artem.fufaldin@gmail.com>
2020-09-02 12:58:06 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,14 @@
 #!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit
+fi
+
+# create fetch job
+source "./fetchmail/create_job.sh"
